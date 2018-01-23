cfc96c9bb22e736de373092be4ae41fee53c71c5
jacchevr <jacchevr@gmail.com>
2018-01-23 10:54:17 -0700

    Added a few notes

--- src/edu/cnm/deepdive/temperature/Converter.java
+++ src/edu/cnm/deepdive/temperature/Converter.java
@@ -1,14 +1,14 @@
 package edu.cnm.deepdive.temperature;
 
 public class Converter {
-
+//using the scratch page we were able use these lines of code without having a main method
   public static double celsiusToFahrenheit(double celsius) {
     return 9 * celsius / 5 + 32;
   }
   
   public static double fahrenheitToCelsius(double fahrenheit) {
     double celsius = 5 * (fahrenheit - 32) / 9;
-        return celsius;
+        return celsius; //remember to return
         
   }
 }
